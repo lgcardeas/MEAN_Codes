b25f30d1d56266148c849f54295adf37749120e5
Luis Ernesto <lgcardenas910526@gmail.com>
2017-03-07 15:54:37 -0500

    This commit message is going to be a little large. First, we start our project using - npm init - command to initialize the project. Second, we added express to our system using - npm install --save express. Third, we added nodemon using - npm install --save nodemon - to check all time for changes in the server, and if it finds something then it restarts the server.

---
+++ basicStartUpNodeAndExpress/app.js
@@ -0,0 +1,14 @@
+var express = require('express'); //We must specify that we going to need 'express' in our system
+
+var app = express();    //Assign to app an express instance or reference (?)
+
+var port  = 8000;     // Set up the port we going to use
+
+app.listen(port, function(err, res){ //Start listening by the port we specified
+    if (err){
+        console.log("An error has occurred");
+    } else {
+        console.log("server started");
+    }
+}); 
+
